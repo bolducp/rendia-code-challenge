9ccf15864c7a2fa0f6d1437a79b03afc60a27aa1
Paige Bolduc <paigebolduc@gmail.com>
2016-11-07 13:27:52 -0500

    Add background transitions to match media.

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-"use strict";
+'use strict';
 
 $(document).ready(init);
 
 function init(){
+    focusOnMedia();
     transitionToNightVideo();
+    fadeToBlackBackground();
     transitionNightImage();
     transitionToDayVideo();
+}
+
+function focusOnMedia() {
+     $('html, body').animate({
+        scrollTop: $('#media_container').offset().top
+    }, 2000);
 }
 
 function transitionToNightVideo() {
@@ -22,6 +30,7 @@ function transitionNightImage() {
 
     setTimeout(function() {
         $("#night").addClass('animated fadeOutRight');
+        fadeToDayBackground();
     }, 22400);
 }
 
@@ -31,4 +40,12 @@ function transitionToDayVideo() {
         $("#to_day_video").show().addClass('animated fadeInLeft');
         $("#to_day_video").get(0).play();
     }, 22800);
+}
+
+function fadeToBlackBackground() {
+    $('#main').animate({backgroundColor: 'black'}, 12000);
+}
+
+function fadeToDayBackground() {
+    $('#main').animate({backgroundColor: '#FFFCF3'}, 12000);
 }
