cb89973ee13293c9c31944f9e65e55107a1cae12
Paige Bolduc <paigebolduc@gmail.com>
2016-11-07 12:33:09 -0500

    Add functionality for transitioning between media.

--- main.js
+++ main.js
@@ -3,18 +3,32 @@
 $(document).ready(init);
 
 function init(){
-  playToDayVideo();
-
+    transitionToNightVideo();
+    transitionNightImage();
+    transitionToDayVideo();
 }
 
-function showImage() {
-
-}
-
-function playToDayVideo() {
-    
+function transitionToNightVideo() {
     setTimeout(function() {
-        $("#to_day_video").get(0).play();
-    }, 10000);
+        $('#to_night_video').addClass('animated fadeOutRight');
+    }, 12000);
+}
 
+function transitionNightImage() {
+    setTimeout(function() {
+        $('#to_night_video').remove();
+        $("#night").show().addClass('animated fadeInLeft');
+    }, 12400);
+
+    setTimeout(function() {
+        $("#night").addClass('animated fadeOutRight');
+    }, 22400);
+}
+
+function transitionToDayVideo() {
+    setTimeout(function() {
+        $('#night').remove();
+        $("#to_day_video").show().addClass('animated fadeInLeft');
+        $("#to_day_video").get(0).play();
+    }, 22800);
 }
